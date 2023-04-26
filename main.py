a5ebcee888ebe4211f5cc262474d8a8a95d9cb7e
BillStephens2022 <two4onebill@yahoo.com>
2023-04-26 16:41:57 -0400

    added routes for a random cafe and to retrieve all cafes - returns a json object with a random cafe or all cafes depending on route

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
+import os
 
 app = Flask(__name__)
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 # Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'cafes.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -23,11 +26,27 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    # method added to Cafe class to convert database to dictionary
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
     
+
+@app.route("/random")
+def get_random_cafe():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(cafes)
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route("/all")
+def get_all_cafes():
+    cafes = db.session.query(Cafe).all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
 
 # HTTP GET - Read Record
 
