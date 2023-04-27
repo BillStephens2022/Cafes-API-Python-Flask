2df75fcfe9673c6eb499d1943466dc59df87995b
BillStephens2022 <two4onebill@yahoo.com>
2023-04-27 10:44:32 -0400

    added route and functionality to update the coffee price on a specific cafe Id.  successfully tested PATCH request in Insomnia

--- main.py
+++ main.py
@@ -87,7 +87,17 @@ def post_new_cafe():
     return jsonify(response={"success": "Successfully added the new cafe."})
 
 
-# HTTP PUT/PATCH - Update Record
+# PATCH REQUEST - for updating price
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def patch_new_price(cafe_id):
+    new_price = request.json.get("coffee_price")
+    cafe = db.session.query(Cafe).get(cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
 
 # HTTP DELETE - Delete Record
 
